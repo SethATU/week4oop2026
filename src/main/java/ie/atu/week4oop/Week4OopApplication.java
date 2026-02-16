cb7094721fe30480b8e8a8497663d66babaa2897
SethButler-STUDENT <g00467621@atu.ie>
2026-02-16 11:13:38 +0000

    initial commit

---
+++ src/main/java/ie/atu/week4oop/Week4OopApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week4oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4OopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week4OopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week4oop/Week4OopApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week4oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4OopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
